e5f1fd80b9dbf71cf2728252d7df93e80bde7bf3
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2024-08-04 08:41:55 +0200

    Update seisinterpy.py

--- seisinterpy.py
+++ seisinterpy.py
@@ -1,3 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+from scipy.interpolate import interpn
+
 def openSegy3D(filename, iline=189, xline=193):
   """
   Open 3D seismic volume in SEGY or SGY format
@@ -88,6 +94,97 @@ def openSegy3D(filename, iline=189, xline=193):
 
   return cube
 
+def sliceViewer(cube, cube_name=" "):
+  """
+  Interactive viewer of 2D slice from a 3D seismic volume data
+
+  NOTE: Interactive display. If you want static display, use: sliceCube
+
+  """
+  from ipywidgets import interact, interactive, fixed, interact_manual, ToggleButtons
+  import ipywidgets as widgets
+  import numpy as np
+  import matplotlib.pyplot as plt
+
+  # Access data and properties of cube
+  data, inlines, crosslines, twt, sample_rate = cube.data, cube.inlines,\
+                                                cube.crosslines, cube.twt,\
+                                                cube.sample_rate                                         
+
+  # Interactive plotting
+  type = ToggleButtons(description='Selection',
+                       options=['Inline','Crossline','Timeslice'])
+  cmap_button = ToggleButtons(description='Colormaps',
+                              options=['gray','seismic','RdBu','PuOr','Accent'])
+  
+  inline_loc = widgets.IntSlider(value=min(inlines), min=min(inlines), 
+                                 max=max(inlines))
+  xline_loc = widgets.IntSlider(value=min(crosslines), min=min(crosslines), 
+                                max=max(crosslines))
+  timeslice_loc = widgets.FloatSlider(value=min(twt), min=min(twt), 
+                                      max=max(twt), step=sample_rate)
+  vmin = widgets.FloatSlider(value=np.amin(data), min=np.amin(data), 
+                             max=np.amax(data))
+  vmax = widgets.FloatSlider(value=np.amax(data), min=np.amin(data), 
+                             max=np.amax(data))
+
+  @interact   
+  def f(type=type, inline_loc=inline_loc, xline_loc=xline_loc,
+        timeslice_loc=timeslice_loc, vmin=vmin, vmax=vmax, cmap=cmap_button):  
+    
+    if type == 'Inline':
+
+      # with segyio.open(filename) as f: 
+      inline_slice = data[inline_loc-inlines[0],:,:]  
+
+      plt.figure(figsize=(20, 10))
+      plt.title('{} Seismic at Inline {}'.format(cube_name, inline_loc))
+      extent = [crosslines[0], crosslines[-1], twt[-1], twt[0]]
+
+      p1 = plt.imshow(inline_slice.T, cmap=cmap, aspect='auto', extent=extent,
+                      vmin=vmin, vmax=vmax, interpolation='bicubic')
+      plt.colorbar()
+
+      plt.xlabel('Crossline'); plt.ylabel('TWT')
+      plt.show()
+
+    if type == 'Crossline':
+
+      # with segyio.open(filename) as f:
+      xline_slice = data[:,xline_loc-crosslines[0],:]  
+
+      plt.figure(figsize=(20, 10))
+      plt.title('{} Seismic at Crossline {}'.format(cube_name, xline_loc))
+      extent = [inlines[0], inlines[-1], twt[-1], twt[0]]
+
+      p1 = plt.imshow(xline_slice.T, cmap=cmap, aspect='auto', extent=extent, 
+                      vmin=vmin, vmax=vmax, interpolation='bicubic')
+      plt.colorbar()
+
+      plt.xlabel('Inline'); plt.ylabel('TWT')
+      plt.show()
+    
+    if type == 'Timeslice':
+
+      id = np.where(twt == timeslice_loc)[0][0]
+      tslice = data[:,:,id]
+
+      plt.figure(figsize=(8,10))
+      plt.title('{} Seismic at Timeslice {} ms'.format(cube_name, timeslice_loc))
+      extent = [inlines[0], inlines[-1], crosslines[-1], crosslines[0]]
+
+      p1 = plt.imshow(tslice.T, cmap=cmap, aspect='auto', extent=extent, 
+                      vmin=vmin, vmax=vmax, interpolation='bicubic')
+      plt.colorbar()
+
+
+      plt.xlabel('Inline'); plt.ylabel('Crossline')
+      plt.gca().xaxis.set_ticks_position('top') # axis on top
+      plt.gca().xaxis.set_label_position('top') # label on top
+      plt.xlim(min(inlines), max(inlines))
+      plt.ylim(min(crosslines), max(crosslines))
+      plt.axis('equal')
+      plt.show()   
 
 def MeanSqAmplitude(x, w):
   return np.convolve(np.square(x), np.ones(w)/w, 'same')
